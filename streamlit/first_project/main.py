9d2fb8f300daca81f1bead47c73bdb06c5c05d48
biamofe <133961615+biamofe@users.noreply.github.com>
2024-04-29 11:58:36 +0200

    prima_impostazione

---
+++ streamlit/first_project/main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.write(df)
+    
+
+
+
