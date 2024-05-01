e516d9a0b41e60a9e61b71ade27eb1b10026e2c5
biamofe <133961615+biamofe@users.noreply.github.com>
2024-05-01 21:24:46 +0200

    .

--- streamlit/first_project/utilities/utilities_lib.py
+++ streamlit/first_project/utilities/utilities_lib.py
@@ -5,4 +5,15 @@ import io
 
 class utility_functions:
     def get_db_connection() -> sqlalchemy.engine.base.Connection:
-        connection = sqlalchemy
+        connection = sqlalchemy.create_engine('sqlite:///db.sqlite')
+        return connection
+
+    def get_data_from_url(ROOT: str, filename:str, format: str, separatore: str) -> pd.DataFrame:
+        URL = f'{ROOT}/{filename}.{format}?raw=true'
+        response = requests.get(URL)
+        if response.status_code == 200:
+            dati = requests.get(URL).content
+            dati_raw = pd.read_csv(io.StringIO(dati.decode('utf-8')), sep=separatore)
+            return dati_raw
+        else:
+            print('Errore nel caricamento dei dati')   
