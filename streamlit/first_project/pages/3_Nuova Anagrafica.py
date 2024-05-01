2eba3eb0247b7ef663057eccd89c5810de697e31
biamofe <133961615+biamofe@users.noreply.github.com>
2024-05-01 16:42:17 +0200

    new_files

--- streamlit/first_project/pages/3_Nuova Anagrafica.py
+++ streamlit/first_project/pages/3_Nuova Anagrafica.py
@@ -1,10 +1,21 @@
 import streamlit as st
 import pandas as pd
 import sqlalchemy
+import requests
+import io
+from utilities.utilities_lib import utility_functions as uf
 
-DATA_PATH = '.data/'
-df_nazioni = pd.read_csv(DATA_PATH + 'elenco_nazioni.csv')
-elenco_nazioni = df_nazioni['nazione'].tolist() 
+#leggi i dati dal seguente path: 'https://github.com/federicobiamonte/MyApps/blob/main/streamlit/first_project/data/stati.csv?raw=true'
+
+DATA_URL ='https://github.com/federicobiamonte/MyApps/blob/main/streamlit/first_project/data/stati.csv?raw=true'
+
+df_nazioni = uf.get_data_from_url(DATA_URL, 'stati', 'csv')
+elenco_nazioni = df_nazioni['nome'].unique().tolist()
+
+df_regioni = uf.get_data_from_url(DATA_URL, 'regioni', 'csv')
+elenco_regioni = df_regioni['denominazione_regione'].unique().tolist()
+
+st.write(df_regioni)
 
 st.title('Modulo Inserimento Anagrafiche', anchor='left')
 
@@ -20,10 +31,24 @@ if tipologia == 'Persona':
         col1, col2 = st.columns(2)
         with col1:
             nome = st.text_input('Nome', value='', max_chars=None, key='textinput_nome', placeholder='')
-
         with col2:
             cognome = st.text_input('Cognome', value='', max_chars=None, key='textinput_cognome')
         
-        st.selectbox('Stato', options = elenco_nazioni, index=None, key='dropdown_stato')
+
+
+        stato = st.selectbox('Stato', options = elenco_nazioni, index=None, key='dropdown_stato')
+        if stato == 'Italia':
+            col3, col4, col5 = st.columns(3)
+            with col3:
+                regione = st.selectbox('Regione', options = elenco_regioni)
+
+    
+
+
+
+
+
 
         st.text_input('Indirizzo', value='', max_chars=None, key='textinput_indirizzo', placeholder='Es: Via Roma, 1 Roma')
+
+
--- streamlit/first_project/utilities/utilities_lib.py
+++ streamlit/first_project/utilities/utilities_lib.py
@@ -1,7 +1,19 @@
 import sqlalchemy
 import pandas as pd
+import requests
+import io
 
 class utility_functions:
-    def get_db_connection():
+    def get_db_connection() -> sqlalchemy.engine.base.Connection:
         connection = sqlalchemy.create_engine('sqlite:///db.sqlite')
         return connection
+
+    def get_data_from_url(ROOT: str, filename:str, format: str) -> pd.DataFrame:
+        URL = f'{ROOT}/{filename}.{format}?raw=true'
+        response = requests.get(URL)
+        if response.status_code == 200:
+            dati = requests.get(URL).content
+            dati_raw = pd.read_csv(io.StringIO(dati.decode('utf-8')))
+            return dati_raw
+        else:
+            print('Errore nel caricamento dei dati')   
